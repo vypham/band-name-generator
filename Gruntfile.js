499ab307239467e975e946fa64eedbe7b4c0abdd
stephanielingwood <stephanielingwood@gmail.com>
2015-06-07 11:15:09 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+'use strict;'
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-jscs");
+
+  var srcFiles = ["app/**/*.js", "lib/**/*.js", "server.js"];
+
+  grunt.initConfig({
+    jshint: {
+      files: srcFiles,
+      options: {
+        globalstrict: true,
+        quotmark: false,
+        smarttabs: true,
+        trailing: true,
+        undef: true,
+        unused: true,
+        indent: 2,
+        node: true
+      }
+    },
+    jscs: {
+      src: srcFiles,
+      options: {
+        preset: "airbnb",
+        requireDotNotation: null,
+        disallowMultipleVarDecl: null,
+        requireMultipleVarDecl: null
+      }
+    }
+  });
+
+  grunt.registerTask("default", [ "jshint", "jscs"]);
+};
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # band-name-generator
+
+A repo for the Foundations II: JavaScript class at Code Fellows.
+
+Need a name for your band, or just a random band name to liven up your day? This is the site for you.
+
+To play with this repo:
+1.) Fork it
+2.) Clone it to your machine
+3.) ```cd``` into the band-name-generator folder
+4.) Run ```npm install```
